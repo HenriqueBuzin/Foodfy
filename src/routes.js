f9e28650b6df4a20ab72624887ee983054fd2d76
Henrique Buzin <cultura.henrique@gmail.com>
2020-04-13 21:15:02 -0300

    Files

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,12 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', function(req, res) {
-    return res.send("instructors")
+    return res.redirect("home")
 })
+
+routes.get('/home', function(req, res) {
+    return res.render("home/index")
+})
+
 
 module.exports = routes
