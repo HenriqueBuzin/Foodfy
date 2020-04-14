4d1833de949d7f8e9775a650c65c14ceaa2d58d0
Henrique Buzin <cultura.henrique@gmail.com>
2020-04-13 22:08:46 -0300

    Controllers

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,28 @@
 const express = require('express')
 const routes = express.Router()
+const home = require("./app/controllers/home")
+const receitas = require("./app/controllers/receitas")
+const sobre = require("./app/controllers/sobre")
 
 routes.get('/', function(req, res) {
     return res.redirect("home")
 })
 
-routes.get('/home', function(req, res) {
-    return res.render("home/index")
-})
+routes.get('/home', home.index)
 
+routes.get('/sobre', sobre.index)
 
-routes.get('/sobre', function(req, res) {
-    return res.render("sobre/index")
-})
+routes.get('/receitas', receitas.index)
 
-routes.get('/receitas', function(req, res) {
-    return res.render("receitas/index")
-})
+/*
+routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
+routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
+routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
+routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
 
+routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
+routes.put("/admin/recipes", recipes.put); // Editar uma receita
+routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
+*/
 
 module.exports = routes
