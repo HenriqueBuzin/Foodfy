610b59e5648415b9b0f9283308514abce8e5be49
Henrique Buzin <cultura.henrique@gmail.com>
2020-04-13 22:44:05 -0300

    Files

---
+++ src/app/controllers/recipes.js
@@ -0,0 +1,23 @@
+module.exports = {
+    index(req, res){
+        return res.render("home/index")
+    },
+    create(req, res){
+        return res.render("home/index")
+    },
+    show(req, res){
+        return res.render("home/index")
+    },
+    edit(req, res){
+        return res.render("home/index")
+    },
+    post(req, res){
+        return res.render("home/index")
+    },
+    put(req, res){
+        return res.render("home/index")
+    },
+    delete(req, res){
+        return res.render("home/index")
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const routes = express.Router()
 const home = require("./app/controllers/home")
 const receitas = require("./app/controllers/receitas")
 const sobre = require("./app/controllers/sobre")
+const recipes = require("./app/controllers/recipes")
+const chefs = require("./app/controllers/chefs")
 
 routes.get('/', function(req, res) {
     return res.redirect("home")
@@ -14,7 +16,6 @@ routes.get('/sobre', sobre.index)
 
 routes.get('/receitas', receitas.index)
 
-/*
 routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
 routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
 routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
@@ -23,6 +24,7 @@ routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de e
 routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
 routes.put("/admin/recipes", recipes.put); // Editar uma receita
 routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
-*/
+
+routes.get('/chefs', chefs.index)
 
 module.exports = routes
